cd59dea23c2d9387430e299cd483926d0e85b2fd
Ahmad Nawid <ahmadnawid.mz@gmail.com>
2024-12-16 17:46:54 +0100

    connected with the frontend and add product with id  view

--- ecom-project/src/main/java/com/app/ecom_project/controller/ProductController.java
+++ ecom-project/src/main/java/com/app/ecom_project/controller/ProductController.java
@@ -3,26 +3,38 @@ package com.app.ecom_project.controller;
 import com.app.ecom_project.model.Product;
 import com.app.ecom_project.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin
 public class ProductController {
 
     @Autowired
     private ProductService service;
 
-    @RequestMapping("/")
+    /*@RequestMapping("/")
     public String greeting(){
         return "Hello Home";
-    }
+    }*/
 
     @GetMapping("/products")
-    public List<Product> getAllProducts(){
-        return service.getAllProducts();
+    public ResponseEntity<List<Product>> getAllProducts(){
+        return new ResponseEntity<>(service.getAllProducts(), HttpStatus.OK);
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable int id){
+
+        Product product = service.getProductById(id);
+
+        if(product != null)
+            return new ResponseEntity<>(service.getProductById(id), HttpStatus.OK);
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
